89cbedcf6bff1e6b115363efbb2264df9cacd4e9
romankozeiko-cmd <romankozeiko@gmail.com>
2025-12-27 01:41:02 +0300

    Create Allusionfree.lua

---
+++ Allusionfree.lua
@@ -0,0 +1,19 @@
+if not game:IsLoaded() then
+    repeat task.wait(3) until game:IsLoaded()
+end
+local Allusion_DarkLoader = "https://raw.githubusercontent.com/darkallusion1/Allusion/refs/heads/main/Loader.lua"
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local PlaceId = game.PlaceId
+local Games = {
+    [11455876520] = "https://api.luarmor.net/files/v4/loaders/97bcf334758e81ee33dc522cf41ccadd.lua",
+}
+local FoundGame = Games[PlaceId] or false
+local FoundGame2 = Allusion_DarkLoader ~= "" and loadstring(game:HttpGet(Allusion_DarkLoader))() or false
+if FoundGame then
+    loadstring(game:HttpGet(FoundGame))()
+elseif FoundGame2 then
+    FoundGame2()
+else
+    Player:Kick("Allusion : Game Not Support")
+end
